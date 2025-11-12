bbf6213de4b9786bbd54fe0172626711bbcf5640
atiugaev <atiugaev@synergy.ru>
2025-11-12 22:00:55 +0300

    HW1 Tyugaev

--- inbox/lib/services/mail_manager.rb
+++ inbox/lib/services/mail_manager.rb
@@ -8,30 +8,53 @@ class MailManager
   def call(&)
     connect
     attachments(emails, &)
+    handle_empty_emails
+  ensure
+    connect.logout
+    connect.disconnect
   end
 
   private
 
   def connect
-    Mail.defaults do
-      retriever_method :imap,
-        address: Settings.imap.server,
-        port: Settings.imap.port,
-        user_name: Settings.imap.email,
-        password: Settings.imap.password,
-        enable_ssl: true
+    @connect ||= begin
+      connection = Net::IMAP.new(Settings.imap.server, Settings.imap.port, true)
+      connection.login(Settings.imap.email, Settings.imap.password)
+      connection.select('INBOX')
+      connection
     end
   end
 
+  def empty_emails_uid
+    @empty_emails_uid ||= []
+  end
+
   def emails
-    Mail.find(keys: %w[NOT SEEN])
+    ids = connect.search(['UNSEEN'])
+
+    return if ids.empty?
+
+    connect.fetch(ids, ['UID', 'RFC822'])
   end
 
   def attachments(emails, &block)
-    emails.each do |email|
-      email.attachments.map do |attachment|
+    emails&.each do |email|
+      uid = email.attr['UID']
+      raw_email_source = email.attr['RFC822']
+      message = Mail.read_from_string(raw_email_source)
+
+      empty_emails_uid << uid and next if message.attachments.empty?
+
+      message.attachments.map do |attachment|
         TempfileManager.call(attachment, &block)
       end
+    end
+  end
+
+  def handle_empty_emails
+    empty_emails_uid.each do |uid|
+      connect.uid_store(uid, '-FLAGS', [:Seen])
+      connect.uid_move(uid, 'errors')
     end
   end
 end
