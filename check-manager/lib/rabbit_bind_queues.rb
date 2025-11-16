22f12dfd7196311e48302804e313845839d08ad9
Igor Simdyanov <i.simdanov@netology.ru>
2025-11-16 23:15:27 +0300

    Подготавливаем dead-letter-queue

--- check-manager/lib/rabbit_bind_queues.rb
+++ check-manager/lib/rabbit_bind_queues.rb
@@ -19,9 +19,17 @@ class RabbitBindQueues
     end
 
     def create_queue_for_worker(exchange, worker_class)
+      if defined? worker_class::QUEUE_DEAD_LETTER
+        dead = channel.queue(
+          worker_class::QUEUE_DEAD_LETTER,
+          durable: true
+        )
+        dead.bind(exchange, routing_key: '#')
+      end
+
       queue = channel.queue(
         worker_class::QUEUE_NAME,
-        durable: true
+        worker_class::ARGUMENTS
       )
       worker_class::ACTION_HANDLERS.each_key do |r_key|
         queue.bind(exchange, routing_key: r_key)
